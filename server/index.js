ded73f50f79ae377d5971fef2207544c24fe8c21
Matthew McQuain <37052988+mattoattacko@users.noreply.github.com>
2021-03-30 20:40:06 -0700

    Update index.js
    removed the url info from the back end

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://mattoattacko:mattmatt7878!@cluster0.gn0wy.mongodb.net/Project0?retryWrites=true&w=majority';
+const CONNECTION_URL = 'URL removed';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
